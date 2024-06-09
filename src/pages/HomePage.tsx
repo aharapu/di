6ab97de5dbd88f866f4ef879c8312f8cc2fc0715
Valentin Harapu <alexandru.harapu@gmail.com>
2024-06-09 14:13:34 +0300

    Enable search bar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,19 +13,25 @@ export const HomePage: React.FunctionComponent = () => {
   const [tableItems, setTableItems] = useState<TableItem[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const getItems = async (search: string) => {
+    setIsLoading(true);
+    const { results } = await getPeople({ search });
+
+    const items = results.map((r) => ({ id: r.url, data: { name: r.name, mass: r.mass } }));
+    setTableItems(items);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    getPeople().then(({ results }) => {
-      setTableItems(results.map((r) => ({ id: r.url, data: { name: r.name, mass: r.mass } })));
-      setIsLoading(false);
-    });
+    getItems('');
 
     return () => {
-      // TODO -> cancel promise
+      // TODO -> cancel active promises
     };
   }, []);
 
   const handleSearch = (val: string) => {
-    console.log('val', val);
+    getItems(val);
   };
 
   return (
@@ -34,7 +40,7 @@ export const HomePage: React.FunctionComponent = () => {
         padding: '40px',
       }}
     >
-      <DiSearch onSearch={handleSearch} />
+      <DiSearch onSearch={handleSearch} disabled={isLoading} />
       <DiTable
         items={tableItems}
         columns={['name', 'mass']}
